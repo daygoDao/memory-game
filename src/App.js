52deac1d6d9f01eca8731e31d86895ce611dfcaa
dao vo <gary.vo32@gmail.com>
2022-05-04 15:56:53 -0700

    added Gallery.js and a folder to add images to

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Gallery from "./Gallery";
 function App() {
   return (
     <div className="App">
       <h1>Memory Game</h1>
+      <Gallery />
     </div>
   );
 }
 
 export default App;
+    
