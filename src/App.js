15dee2223fe0342cf66e9787b0bfdef39615f26c
dao vo <gary.vo32@gmail.com>
2022-05-04 20:31:27 -0700

    added static pictures to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Gallery from "./Gallery";
+import './style.css'
+
 function App() {
   return (
     <div className="App">
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,7 +1,42 @@
 import React from "react";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const level1 = [
+    "brook",
+    "chopper",
+    "franky",
+    "god",
+    "jinbe",
+    "luffy",
+    "nami",
+    "robin",
+    "sanji",
+    "zoro",
+  ];
+
+  let round = [];
+  for (let person of level1) {
+    round.push(<Card name={person} />);
+  }
+
+  return (
+    <ul>
+      <h1>yo</h1>
+      {level1.map( x => <Card name={x} /> )}
+    </ul>
+  );
+};
+
+const Card = ({name}) => {
+  return (
+    <li className="card">
+      <img
+        src={`./images/wanted/${name}.png 
+      `}
+        alt={name}
+      />
+    </li>
+  );
 };
 
 export default Gallery;
