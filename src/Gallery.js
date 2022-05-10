e95092458aa771c5458991068888c26887c638de
dao vo <gary.vo32@gmail.com>
2022-05-10 14:24:54 -0700

    able to restart game when all levels are complete

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,21 +2,14 @@ import { useState, useEffect } from "react";
 import Card from "./Card";
 import levelDB from "./levels";
 
-const Gallery = ({
-  bestScore,
-  setBest,
-  // currLevel,
-  // setNewLevel,
-  setNewScore,
-}) => {
+const Gallery = ({ bestScore, setBest, setNewScore }) => {
   const [currLevel, setNewLevel] = useState(0);
   const [level, setLevel] = useState(levelDB.levels[currLevel]);
   const [levelScore, setLevelScore] = useState([]);
 
   useEffect(() => {
-    console.log("within useEffect #1");
+    console.log("within useEffect #1: check if level is done");
     console.log(levelScore);
-    // check if level is done
     if (levelScore.length == level.people.length) {
       console.log("done");
       setNewLevel((prev) => prev + 1);
@@ -24,18 +17,19 @@ const Gallery = ({
   }, [level, levelScore]);
 
   useEffect(() => {
-    console.log("within useEffect #2");
-    setLevelScore([]);
-    handleLevels();
-  }, [currLevel]);
-
-  const handleLevels = () => {
-    setLevel({ ...levelDB.levels[currLevel] });
-
     console.log("currLevel is: " + currLevel);
-    console.log(levelScore);
     console.log(levelDB.levels[currLevel]);
-  };
+    if (currLevel > levelDB.levels.length - 1) {
+      console.log("game is finished");
+      alert('finished game, continue to add more to your score!')
+      setNewLevel(0)
+      setLevel({ ...levelDB.levels[0] });
+    } else {
+      console.log("within useEffect #2: currLevel updated");
+      setLevel({ ...levelDB.levels[currLevel] });
+    }
+    setLevelScore([]);
+  }, [currLevel]);
 
   const display = level.people.map((x, i) => (
     <Card
